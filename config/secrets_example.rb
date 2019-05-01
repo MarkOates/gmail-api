619a1afc3e8f071928417fc90f7da90ade50d004
Mark Oates <mark.oates@shopify.com>
2019-05-01 16:28:44 -0400

    Setup better secrets and tokens management

---
+++ config/secrets_example.rb
@@ -0,0 +1,7 @@
+class Secrets
+  GMAIL_API_CREDENTIALS_PATH = 'config/secrets/name-of-your-gmail-api-credentials-file.json'.freeze
+  GMAIL_API_TOKEN_PATH = 'config/secrets/tokens/gmail-api.token.yaml'.freeze
+
+  GOOGLE_DOCS_CREDENTIALS_PATH = 'config/secrets/name-of-your-google-docs-credentials-file.json'.freeze
+  GOOGLE_DOCS_TOKEN_PATH = 'config/secrets/tokens/google-docs-api.token.yaml'.freeze
+end
--- lib/google_docs_api.rb
+++ lib/google_docs_api.rb
@@ -6,11 +6,13 @@ require 'googleauth/stores/file_token_store'
 require 'fileutils'
 require 'pry'
 
+require_relative '../config/secrets.rb'
+
 class GoogleDocsApi
   OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
   APPLICATION_NAME = 'Google Docs API Ruby Quickstart'.freeze
-  CREDENTIALS_PATH = 'credentials.json'.freeze
-  TOKEN_PATH = 'token.yaml'.freeze
+  CREDENTIALS_PATH = Secrets::GOOGLE_DOCS_CREDENTIALS_PATH
+  TOKEN_PATH = Secrets::GOOGLE_DOCS_TOKEN_PATH
   SCOPE = Google::Apis::DocsV1::AUTH_DOCUMENTS_READONLY
 
   def document(document_id:)
