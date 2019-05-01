21f967a3a0c39be4931d0ec79e23db4fb9f1954a
Mark Oates <markoates0@gmail.com>
2019-04-30 23:46:53 -0400

    ⚠️ PII - Add passing test data

--- test/gmail_api_test.rb
+++ test/gmail_api_test.rb
@@ -6,7 +6,7 @@ class GmailApiTest < Minitest::Test
   def test_labels_returns_my_email_labeles
     gmail_api = GmailApi.new
     expected_labels = {
-      "key" => "value",
+"Fullscore"=>"Label_14", "CATEGORY_FORUMS"=>"CATEGORY_FORUMS", "Music Library"=>"Label_4", "Student Loans"=>"Label_9", "Family"=>"Label_8", "[Imap]/Sent"=>"Label_2", "Friends"=>"Label_7", "CATEGORY_PERSONAL"=>"CATEGORY_PERSONAL", "CATEGORY_SOCIAL"=>"CATEGORY_SOCIAL", "[Imap]/Drafts"=>"Label_1", "[Imap]/Trash"=>"Label_3", "Contacts"=>"Label_11", "IMPORTANT"=>"IMPORTANT", "Payments Sent / Recipts"=>"Label_10", "Adobe Customer Service Can cancel after any two months"=>"Label_13", "Shopify"=>"Label_12", "Payments Received"=>"Label_5", "CATEGORY_UPDATES"=>"CATEGORY_UPDATES", "CHAT"=>"CHAT", "SENT"=>"SENT", "INBOX"=>"INBOX", "TRASH"=>"TRASH", "CATEGORY_PROMOTIONS"=>"CATEGORY_PROMOTIONS", "DRAFT"=>"DRAFT", "SPAM"=>"SPAM", "STARRED"=>"STARRED", "UNREAD"=>"UNREAD"
     }
 
     assert_equal expected_labels, gmail_api.labels
@@ -15,7 +15,7 @@ class GmailApiTest < Minitest::Test
   def test_threads_returns_the_threads
     gmail_api = GmailApi.new
     expected_threads = [
-      "string",
+      "https://openai.com/blog/musenet/", "Hymns of Poseidon - Maple &amp; Rye", "", "On Sun, Nov 5, 2017 at 10:56 PM, Mark Oates &lt;markoates0@gmail.com&gt; wrote: On Sun, Nov 5, 2017 at 10:50 PM, Mark Oates &lt;markoates0@gmail.com&gt; wrote: Pilot Experiment On Verbal Attributes"
     ]
 
     assert_equal expected_threads, gmail_api.threads
